15b29296e1caae8741831f22fc68d3cc076c2c16
yangblink <yangblink@gmail.com>
2018-11-04 22:15:55 +0800

    backup

--- src/views/layout/components/Sidebar/index.vue
+++ src/views/layout/components/Sidebar/index.vue
@@ -1,20 +1,13 @@
 <template>
-	<scroll-bar>
-		<el-menu
-			class="sidebar-container"
-			mode="vertical"
-			:collapse="collapse"
-		>
-			<el-menu-item v-for="(item, index) in routers" :index="index.toString()" :key="index">
-				<i v-if="item.icon" :class="'el-icon-' + item.icon"></i>
-				<span slot="title">{{item.title}}</span>
-			</el-menu-item>
-			<el-button type="error" @click="test">sadf</el-button>
-		</el-menu>
-	</scroll-bar>
+	<el-menu class="sidebar-container" mode="vertical" :collapse="collapse">
+		<el-menu-item v-for="(item, index) in routers" :index="index.toString()" :key="index">
+			<i v-if="item.icon" :class="'el-icon-' + item.icon"></i>
+			<span slot="title">{{item.title}}</span>
+		</el-menu-item>
+		<el-button type="error" @click="test">sadf</el-button>
+	</el-menu>
 </template>
 <script>
-import ScrollBar from '@/components/ScrollBar'
 export default {
 	data() {
 		return {
@@ -32,7 +25,6 @@ export default {
 			}]
 		}
 	},
-	components: { ScrollBar },
 	methods: {
 		test() {
 			this.collapse = !this.collapse
@@ -42,7 +34,8 @@ export default {
 </script>
 <style>
 .sidebar-container:not(.el-menu--collapse) {
-	flex: 0 0 200px;
+	flex-grow: 0;
+	flex-shrink: 0;
 	min-height: 100vh;
 	width: 200px;
 }
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,8 +1,37 @@
 <template>
-	<div>
-		<h1>login</h1>
+	<div class="login-container">
+		<div class="form-container">
+			<el-form ref="loginForm" :model="form" label-width="80px">
+				<el-form-item label="用户名">
+					<el-input v-model="form.username"></el-input>
+				</el-form-item>
+				<el-form-item label="密码">
+					<el-input v-model="form.password"></el-input>
+				</el-form-item>
+			</el-form>
+		</div>
 	</div>
 </template>
 <script>
-	export default {}
+	export default {
+		data() {
+			return {
+				form: {
+					username: '',
+					password: ''
+				}
+			}
+		}
+	}
 </script>
+<style>
+	.login-container {
+		position: fixed;
+		display: flex;
+		height: 100%;
+		width: 100%;
+		background-color: #2d3a4b;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
